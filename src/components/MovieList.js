016d93881742f968f2c2c0484f90b0677850cb7d
mitchelle <mitchellenjoki7@gmail.com>
2022-07-31 20:10:24 +0300

    add img tag for poster

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,3 +1,20 @@
 import React from 'react'
 const MovieList = ( props, movies) => {
     const FavouriteComponent = props.favouriteComponent
+        return (
+        <>
+           
+           {props.movies.map((movie, index) => (
+           <div
+            className='image-container d-flex justify-content-start m-0'>
+             <img src={movie.Poster} alt='movie img'></img>
+             <div onClick={() => props.handleFavouritesClick (movie)}
+               className="overlay d-flex align-items-center justify-content-center">
+                <FavouriteComponent />
+             </div>
+           </div>
+           ))}
+        </>
+    )
+}
+export default MovieList
